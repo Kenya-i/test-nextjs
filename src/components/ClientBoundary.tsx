a9357bab47e2904a02bd619dc0dc77f975992061
kenya <ken8116@icloud.com>
2026-06-14 05:08:16 +0900

    ClientBoundary/CompositionPattern completed

--- src/components/ClientBoundary.tsx
+++ src/components/ClientBoundary.tsx
@@ -1,8 +1,12 @@
 "use client"
-import React, { useState } from 'react'
+import React, { ReactNode, useState } from 'react'
 import { Box } from './Box';
 
-const ClientBoundary = () => {
+type PropsType = {
+    children: ReactNode;
+}
+
+const ClientBoundary = ({children}: PropsType) => {
     const [isVisible, setIsVisible] = useState(false);
     const toggleVisibility = () => {
         setIsVisible((prevIsVisible) => !prevIsVisible);
@@ -10,7 +14,8 @@ const ClientBoundary = () => {
   return (
     <Box>
         <h2>Client Boundary</h2>
-        <button>{isVisible ? "非表示にする" : "表示にする"}</button>
+        <button onClick={toggleVisibility}>{isVisible ? "非表示にする" : "表示にする"}</button>
+        {isVisible &&  children}
     </Box>
   )
 }
